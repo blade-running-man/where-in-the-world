41160c7d8eb33e658d5c206f6a64016ffbebd8fc
Oleksandr S <oleksandr.slukovskyi@gmail.com>
2022-04-25 23:11:01 +0200

    🏗 - Setup "react-query"

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,25 @@
 import React from 'react'
+import { QueryClient, QueryClientProvider } from 'react-query'
 import { ThemeProvider } from 'styled-components'
 import GlobalStyle from '@styles/GlobalStyles'
 import { theme } from '@styles/Theme'
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+})
+
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <h1>Hello world</h1>
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <h1>Hello world</h1>
+      </ThemeProvider>
+    </QueryClientProvider>
   )
 }
 
